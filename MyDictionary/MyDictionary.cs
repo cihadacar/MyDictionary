c7a7572f65ec1ca390823f4e61747020efa479cb
Cihad Acar <cihadacar1@hotmail.com>
2021-01-27 21:20:02 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, T2>
+    {
+        T[] keys;
+        T2[] values;
+        //constructor
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new T2[0];
+        }
+        
+        public void Add(T key, T2 value)
+        {
+            T[] tempKeys = keys;
+            T2[] tempValues = values;
+
+            keys = new T[keys.Length + 1];
+            values = new T2[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+            
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[values.Length - 1] = value;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("İsim: " + keys[i] + " - Doğum günü: " + values[i]);
+            }
+        }
+        
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, double> birthdays = new MyDictionary<string , double>();
+            birthdays.Add("Cihad", 27.01);
+            birthdays.Add("Muhammet", 30.12);
+            birthdays.Add("Enes", 12.01);
+            birthdays.Add("Eren", 30.07);
+            birthdays.Add("Salih", 18.06);
+            birthdays.Add("Onur", 19.06);
+            birthdays.Add("İlke", 07.08);
+
+            birthdays.Print();
+
+        }
+    }
+}
